d22b966ea433336b14c90235dfe56ac55ab6536d
lebowvsky <lebowvsky@gmail.com>
2019-11-27 00:00:42 +0100

    Design du carousel dans la page Nos Pommes

--- view/frontend/pages/pommes/pommes_description.php
+++ view/frontend/pages/pommes/pommes_description.php
@@ -4,7 +4,7 @@
     <article id="qui_sommes_nous" class="article">
         <h2>Qui sommes-nous ?</h2>
         <section>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora suscipit vel quam, deserunt fugit assumenda molestias recusandae in, adipisci at asperiores commodi pariatur consequuntur mollitia nostrum et repudiandae consectetur culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque dolorum perferendis, fugit, nam excepturi quidem facere praesentium id, ullam voluptas nobis explicabo eum odio corrupti consequuntur quam eaque. Minus, accusamus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore amet sequi totam quia quidem at blanditiis error quod doloremque exercitationem repudiandae ducimus, commodi dolore aliquam magni magnam aperiam nisi consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quis exercitationem tempora quae reiciendis temporibus! Nisi dicta nesciunt in! Est veniam numquam neque iure corporis fugit deleniti fuga asperiores itaque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit reiciendis excepturi modi totam, nostrum nesciunt aspernatur itaque quaerat voluptatibus, voluptas architecto alias tempore deleniti consequuntur, sequi vitae assumenda eligendi esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, assumenda optio distinctio excepturi nulla iure deserunt adipisci exercitationem veritatis alias aliquid debitis nam explicabo commodi nihil deleniti ipsum mollitia similique. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum dolor, asperiores aut architecto reprehenderit pariatur non! Minus, praesentium iste aperiam pariatur ipsam dolore obcaecati veritatis dolorem omnis cumque ipsa reiciendis?</p>
+            <p>Tempora suscipit vel quam, deserunt fugit assumenda molestias recusandae in, adipisci at asperiores commodi pariatur consequuntur mollitia nostrum et repudiandae consectetur culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque dolorum perferendis, fugit, nam excepturi quidem facere praesentium id, ullam voluptas nobis explicabo eum odio corrupti consequuntur quam eaque. Minus, accusamus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore amet sequi totam quia quidem at blanditiis error quod doloremque exercitationem repudiandae ducimus, commodi dolore aliquam magni magnam aperiam nisi consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quis exercitationem tempora quae reiciendis temporibus! Nisi dicta nesciunt in! Est veniam numquam neque iure corporis fugit deleniti fuga asperiores itaque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit reiciendis excepturi modi totam, nostrum nesciunt aspernatur itaque quaerat voluptatibus, voluptas architecto alias tempore deleniti consequuntur, sequi vitae assumenda eligendi esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, assumenda optio distinctio excepturi nulla iure deserunt adipisci exercitationem veritatis alias aliquid debitis nam explicabo commodi nihil deleniti ipsum mollitia similique. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum dolor, asperiores aut architecto reprehenderit pariatur non! Minus, praesentium iste aperiam pariatur ipsam dolore obcaecati veritatis dolorem omnis cumque ipsa reiciendis?</p>
         </section>
 
     </article>
@@ -14,13 +14,53 @@
         <section>
             <div id="carousel" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active"> <img alt="" src="../contents/images/accueil/carousel/1.jpg"><p class="carousel-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aliquam porro nulla non accusamus labore qui quisquam? Dolorum ipsam neque sequi corporis ipsa consectetur nihil et dolor, voluptate, tempore repellendus.</p></div>
-                    <div class="carousel-item"> <img alt="" src="../contents/images/accueil/carousel/2.jpg"></div>
-                    <div class="carousel-item"> <img alt="" src="../contents/images/accueil/carousel/3.jpg"></div>
-                    <div class="carousel-item"> <img alt="" src="../contents/images/accueil/carousel/4.jpg"></div>
-                    <div class="carousel-item"> <img alt="" src="../contents/images/accueil/carousel/5.jpg"></div>
-                    <div class="carousel-item"> <img alt="" src="../contents/images/accueil/carousel/6.jpg"></div>
-                    <div class="carousel-item"> <img alt="" src="../contents/images/accueil/carousel/7.jpg"></div>
+                    <div class="carousel-item active">
+                        <img alt="" src="../contents/images/accueil/carousel/1.jpg">
+                        <div class="carousel-caption">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero sint dolorem excepturi dolores eos adipisci! A tempora odio velit consectetur modi earum enim, facilis, dolorum adipisci, quibusdam quae ullam pariatur?</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img alt="" src="../contents/images/accueil/carousel/2.jpg">
+                        <div class="carousel-caption">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis, veritatis! Distinctio tempore quaerat ipsum deleniti enim saepe eum alias, ipsa rerum laborum. Esse harum vero, adipisci repellendus ratione debitis animi!</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img alt="" src="../contents/images/accueil/carousel/3.jpg">
+                        <div class="carousel-caption">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quisquam, impedit consectetur ipsa quibusdam.</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img alt="" src="../contents/images/accueil/carousel/4.jpg">
+                        <div class="carousel-caption">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate ipsum nesciunt eum pariatur nostrum laboriosam fugit? Aliquid esse inventore corporis iste dolor quam adipisci nemo iusto delectus placeat, odio debitis?</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img alt="" src="../contents/images/accueil/carousel/5.jpg">
+                        <div class="carousel-caption">
+                            <p>Rem possimus voluptatum laborum non explicabo? Accusantium, qui laboriosam. Assumenda, et autem? Enim dolor debitis, itaque ad eligendi id rerum modi voluptates!</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img alt="" src="../contents/images/accueil/carousel/6.jpg">
+                        <div class="carousel-caption">
+                            <p>Libero sint dolorem excepturi dolores eos adipisci!</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img alt="" src="../contents/images/accueil/carousel/7.jpg">
+                        <div class="carousel-caption">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero sint dolorem excepturi dolores eos adipisci! A tempora odio velit consectetur modi earum enim, facilis, dolorum adipisci, quibusdam quae ullam pariatur?</p>
+                        </div>
+                    </div>
                 </div>
                 <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
