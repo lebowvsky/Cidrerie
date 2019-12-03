48320a331f0907bb02eccddec03bb48cff7bf30f
lebowvsky <lebowvsky@gmail.com>
2019-12-04 00:19:03 +0100

    Amélioration mise en forme page Pommes et ajout de la bonne adresse facebook dans Contact

--- view/frontend/pages/contact/contact.php
+++ view/frontend/pages/contact/contact.php
@@ -28,7 +28,7 @@
         <iframe id="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6528.931850063268!2d-1.7067730389100175!3d47.83640807367243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480f167645543f1d%3A0x4f713da3aa650433!2s21%20La%20Cochardais%2C%2035470%20Bain-de-Bretagne!5e0!3m2!1sfr!2sfr!4v1574609912805!5m2!1sfr!2sfr" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
 
-        <a href="https://www.facebook.com/"><i class="fab fa-facebook fa-4x"></i></a>
+        <a href="https://www.facebook.com/cidre.baindebretagne/"><i class="fab fa-facebook fa-4x"></i></a>
     </div>
 <?php $content = ob_get_clean(); ?>
 
